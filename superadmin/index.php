9e19c10e2286a81a8ccdf8dea73e67ee403eeebc
arielle <ariellevalmonte@gmail.com>
2019-03-25 16:34:39 +0800

    superadmin

--- superadmin/index.php
+++ superadmin/index.php
@@ -1,10 +1,19 @@
 <?php include "includes/admin_header.php"; ?>
-
 <?php 
-	if(!isset($_SESSION['adminname']) && !isset($_SESSION['password'])){
-		session_destroy();
-		header('location: ../Signin/loginadmin.php?error=Login to access.');
-		}
+
+include('../SignIn/serverAdmin.php');
+if (!isAdmin()) {
+    $_SESSION['msg'] = "You must log in first";
+    header('location: ../signIn/loginAdmin.php');
+}   
+
+
+if(!isset($_SESSION['adminname']) && !isset($_SESSION['password'])){
+	session_destroy();
+	header('location: ../Signin/loginadmin.php?error=Login to access.');
+    }
+        
+
  ?>
 
 
@@ -53,8 +62,36 @@
             <ul class="list-unstyled components">
             <li class="active">
                     <a href="index.php">Dashboard</a>
-                </li>             
-           
+                </li>                <li>
+                    <a href="#memberSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Members</a>
+                    <ul class="collapse list-unstyled" id="memberSubmenu">
+                        <li>
+                            <a href="memberlist.php">Membership List</a>
+                        </li>
+                        <li>
+                            <a href="userlist.php">Membership Status</a>
+                        </li>
+                        <li>
+                            <a href="transhistory.php">Membership Transactions</a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                <a href="#adminSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Admins</a>
+                    <ul class="collapse list-unstyled" id="adminSubmenu">
+                        <li>
+                            <a href="adminlist.php">Admin List</a>
+                        </li>
+                        <li>
+                            <a href="editadmin.php">Edit Admin</a>
+                        </li>
+                        <li>
+                            <a href="createadmin.php">Create Admin</a>
+                        </li>
+
+
+                    </ul>
+                </li>
                 <li>
                     <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Blog</a>
                     <ul class="collapse list-unstyled" id="pageSubmenu">
@@ -86,7 +123,7 @@
                     <a href="profile.php" class="btn p-2 mr-2 mb-2  download" style="color:black; font-weight:bold;">Profile</a>
                 </li>
                 <li>
-                    <a class="btn p-2 mr-2 mb-2 btn-danger article" href="../admin/logout.php" style="color:white; font-weight:bold;">Logout</a>
+                    <a class="btn p-2 mr-2 mb-2 btn-danger article" href="../superadmin/logout.php" style="color:white; font-weight:bold;">Logout</a>
                 </li>
             </ul>
         </nav>
@@ -219,8 +256,215 @@
                                 </div>
                     </div>
                 </div>
-              
+                <?php 
 
+
+require_once 'Reports.php';
+
+$current_day = date('d');
+$current_month = date('F');
+$current_year = date('Y');
+
+$yearly_sales = Reports::get_yearly_sales();
+$yearly_title = "$current_month " . ($current_year - 1) .  "-$current_year";
+
+$monthly_sales = Reports::get_monthly_sales();
+$monthly_title = $monthly_sales[0]['SalesMonth'] . " " . $monthly_sales[0]['SalesDay'] . " - " . end($monthly_sales)['SalesMonth'] . " " . end($monthly_sales)['SalesDay'];
+
+$weekly_sales = Reports::get_weekly_sales();
+$week_title =  $weekly_sales[0]['SalesDate'] . " - " . end($weekly_sales)['SalesDate'];
+
+ 
+      ?>
+
+<div class="border rounded shadow-lg" style="font-family: Montserrat, sans-serif;padding-top: 0px;background-color: #ffffff;">
+            <ul class="nav nav-tabs">
+                <li class="nav-item"><a class="nav-link active" role="tab" data-toggle="tab" href="#tab-1" style="font-weight: bold;">Weekly</a></li>
+                <li class="nav-item"><a class="nav-link" role="tab" data-toggle="tab" href="#tab-2" style="font-weight: bold;">Monthly</a></li>
+                <li class="nav-item"><a class="nav-link" role="tab" data-toggle="tab" href="#tab-3" style="font-weight: bold;">Yearly</a></li>
+            </ul>
+            <div class="tab-content" style="background-color: #ffffff;">
+                <div class="tab-pane  active" role="tabpanel" id="tab-1">
+                    <div class="col-md-12 search-table-col" data-aos="fade-up" data-aos-once="true" style="margin-top: 30px;padding-top: 0px;font-family: Montserrat, sans-serif;">
+                        <div class="form-group pull-right col-lg-4">
+                        </div><span class="counter pull-right" ></span>
+                            <br>
+                        <h1 style=" text-align: center;" >Weekly Sales Report</h1>
+                        <h5 style=" text-align: center;" ><?php echo $week_title ?></h5>
+
+                        <div class="table-responsive shadow-lg" style="background-color: #ffffff;height: 400px;margin: 5px;">
+                        <div class="container" style="width:1500px;">
+                        <div id="week">
+                <div class="row">
+                        <canvas id="weeklyChart"></canvas>
+                </div>
+                </div>
+            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="tab-pane" role="tabpanel" id="tab-2">
+                    <div class="col-md-12 search-table-col" data-aos="fade-up" data-aos-once="true" style="margin-top: 30px;padding-top: 0px;font-family: Montserrat, sans-serif;">
+                        <div class="form-group pull-right col-lg-4">
+                    </div><span class="counter pull-right"></span>
+                    <br>
+                        <h1 style=" text-align: center;" >Monthly Sales Report</h1>
+                        <h5 style=" text-align: center;" ><?php echo $monthly_title ?></h5>
+                        <div class="table-responsive shadow-lg" style="background-color: #ffffff;height: 400px;margin: 5px;">
+                        <div class="container" style="width:1500px;">
+
+                        <div id="month">
+        <div class="row">
+            
+                <canvas id="monthlyChart"></canvas>
+            </div>
+
+</div></div>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="tab-pane" role="tabpanel" id="tab-3">
+                    <div class="col-md-12 search-table-col" data-aos="fade-up" data-aos-once="true" style="margin-top: 30px;padding-top: 0px;font-family: Montserrat, sans-serif;">
+                        <div class="form-group pull-right col-lg-4">
+                        </div><span class="counter pull-right"></span>
+                        <br>
+                        <h1 style=" text-align: center;" >Yearly Sales Report</h1>
+                        <h5 style=" text-align: center;" ><?php echo $yearly_title ?></h5>                        <div class="table-responsive shadow-lg" style="background-color: #ffffff;height: 400px;margin: 5px;">
+                        <div class="container" style="width:1500px;">
+
+<div id="year" >
+        <canvas id="yearlyChart" ></canvas>
+
+    </div></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+           
+        </div>
+
+</div>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js"></script>
+        <script>
+            var yearly_chart = document.getElementById('yearlyChart').getContext('2d');
+            var chart = new Chart(yearly_chart, {
+                // The type of chart we want to create
+                type: 'bar',
+                // The data for our dataset
+                data: {
+                    labels: [
+                        <?php foreach($yearly_sales as $key => $item){echo '"' . $item['SalesMonth'] . ' ('. $item['SalesYear'] . ')",';} ?>
+                    ],
+
+                    datasets: [{
+                        label: "Sales",
+                        borderColor: 'green',
+                        backgroundColor: 'blue',
+                        data: [
+                            <?php foreach($yearly_sales as $key => $item){echo $item['TotalPrice'] . ',';} ?>
+                        ],
+                    }]
+                },
+
+                // Configuration options go here
+                options: {
+                    title: {
+                        display: true,
+                        fontSize: 18,
+                        fontString: 'sans-serif',
+                        fontColor: 'blue'
+
+                    },
+                   
+                }
+            });
+
+
+
+            var monthly_chart = document.getElementById('monthlyChart').getContext('2d');
+            var chart = new Chart(monthly_chart, {
+                // The type of chart we want to create
+                type: 'line',
+                // The data for our dataset
+                data: {
+                    labels: [
+                        <?php foreach($monthly_sales as $key => $item){echo '"' . $item['SalesMonth'] . ' '. $item['SalesDay'] . '",';} ?>
+                    ],
+
+                    datasets: [{
+                        label: "Sales",
+                        borderColor: 'blue',
+                        backgroundColor: 'green',
+                        data: [
+                            <?php foreach($monthly_sales as $key => $item){echo $item['TotalPrice'] . ',';} ?>
+                        ],
+                    }]
+                },
+
+                // Configuration options go here
+                options: {
+                    title: {
+                        display: true,
+                        fontSize: 18,
+                        fontString: 'sans-serif',
+                        fontColor: 'green'
+                    },
+                   
+                }
+            });
+
+
+            var weekly_chart = document.getElementById('weeklyChart').getContext('2d');
+            var chart = new Chart(weekly_chart, {
+                // The type of chart we want to create
+                type: 'horizontalBar',
+                // The data for our dataset
+                data: {
+                    labels: [
+                        <?php foreach($weekly_sales as $key => $item){echo '"' . $item['SalesDay'] . ' '. $item['SalesDate'] . '",';} ?>
+                    ],
+
+                    datasets: [{
+                        label: "Sales",
+                        borderColor: 'yellow',
+                        backgroundColor: 'orange',
+                        data: [
+                            <?php foreach($weekly_sales as $key => $item){echo $item['TotalPrice'] . ',';} ?>
+                        ],
+                    }]
+                },
+
+                // Configuration options go here
+                options: {
+                    title: {
+                        display: true,
+                        fontSize: 18,
+                        fontString: 'sans-serif',
+                        fontColor: 'orange',
+
+                    },
+                   
+                }
+            });
+        </script>
+        <script src="js/jquery-2.1.1.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/classie.js"></script>
+        <script src="js/cbpAnimatedHeader.js"></script>
+        <script src="js/wow.min.js"></script>
+        <script src="js/inspinia.js"></script>
+        <script src="js/pace.min.js"></script>
+        <script src="js/login.js"></script>
+    </div>
+
+    </div>
                   <?php
                   $query = "SELECT * FROM posts WHERE post_status ='Draft'";  //Draft Posts
                   $get_draft_posts = mysqli_query($connection, $query);
